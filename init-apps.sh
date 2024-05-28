638ffa44c17bdcc997ac390f8ddbcb568ca29ca3
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2024-05-28 12:09:34 -0700

    add musescore

--- init-apps.sh
+++ init-apps.sh
@@ -20,3 +20,7 @@ open obsidian.AppImage
 # install armcord
 flatpak install flathub xyz.armcord.ArmCord -y
 
+# install musescore
+curl -L -o musescore.AppImage https://cdn.jsdelivr.net/musescore/v4.3.0/MuseScore-Studio-4.3.0.241231431-x86_64.AppImage
+open musescore.AppImage
+
