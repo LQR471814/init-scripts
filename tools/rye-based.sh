5887da25e209437b52272a3d7a7265544512b490
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2024-06-15 12:19:18 -0700

    fix bug

--- tools/rye-based.sh
+++ tools/rye-based.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 # setup ytmdl
-python3 -m pip install ytmdl
+# python3 -m pip install ytmdl
 
 YTMDL_LOCATION=$(python3 -m pip show ytmdl | grep -Eo 'Location:\s*(.*)' | awk '{print $2}')
 PYTHON_LOCATION=$(which python3)
 
 echo "#!/bin/bash
-python3 '$YTMDL_LOCATION/ytmdl/main.py' \$@" > ~/bin/ytmdl
+cd $YTMDL_LOCATION/ytmdl && \
+python3 main.py \$@" > ~/bin/ytmdl
 chmod +x ~/bin/ytmdl
 
