22797e72eefca42adf64fa88adbf0dc09a09b768
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2024-10-14 09:26:42 -0700

    update fonts

--- install-fonts.sh
+++ install-fonts.sh
@@ -1,3 +1,11 @@
+curl -L -o monaspace.zip https://github.com/githubnext/monaspace/releases/download/v1.101/monaspace-v1.101.zip
+unzip -qq monaspace.zip
+rm -rf __MACOSX
+cd monaspace-v1.101
+./util/install_linux.sh
+cd ..
+rm -rf monaspace-v1.101
+
 curl -L -o jetbrainsmono.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/JetBrainsMono.zip
 unzip -qq jetbrainsmono.zip -d jetbrainsmono
 mkdir -p ~/.local/share/fonts
