23cbc61517546cda0bd12699897e32098df5642c
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2024-10-14 13:33:43 -0700

    add activitywatch to dwm autostart instead of systemctl daemon
    this is because the afk watcher requires x server to be started before
    it can connect, the systemctl daemon starts before the x server so the
    afk watcher dies on startup

--- install-services.sh
+++ install-services.sh
@@ -16,12 +16,10 @@ cp ./configs/metasearch-config.toml $metasearch2/config.toml
 start_daemon metasearch2
 
 # setup activitywatch
-activitywatch=$(create_daemon activitywatch "watch your activity, like screen time but for linux" "aw-qt --no-gui")
-
 curl -L -o activitywatch.zip https://github.com/ActivityWatch/activitywatch/releases/download/v0.13.2/activitywatch-v0.13.2-linux-x86_64.zip
 unzip activitywatch.zip
 rm activitywatch.zip
 mv activitywatch ~/bin/
 
-start_daemon activitywatch
+echo "/home/lqr471814/bin/activitywatch/aw-qt --no-gui 2> ~/activitywatch.log &" >> ~/.dwm/autostart.sh
 
