3caebf9bac79494ef67473cbd159e3325b06723c
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2024-11-06 18:19:22 -0800

    add keepass sync script

--- install-gdrive.sh
+++ install-gdrive.sh
@@ -26,4 +26,32 @@ else
 fi" > ~/bin/gdrive
 chmod +x ~/bin/gdrive
 
+echo "#!/bin/zsh
+
+cp ~/gdrive-shared/Passwords.kdbx ~/Downloads/Passwords.kdbx.old
+
+echo 'Pulling remote database...'
+rclone sync 'gdrive:shared/' ~/gdrive-shared
+if [ \$? -eq 0 ]; then
+    echo 'success.'
+else
+    rclone config reconnect gdrive:
+    exit 1
+fi
+
+echo 'Merging local and remote...'
+keepassxc-cli merge -s ~/Downloads/Passwords.kdbx.old ~/gdrive-shared/Passwords.kdbx
+mv ~/Downloads/Passwords.kdbx.old ~/gdrive-shared/Passwords.kdbx
+
+echo 'Pushing merged database...'
+rclone sync ~/gdrive-shared 'gdrive:shared/'
+if [ \$? -eq 0 ]; then
+    echo 'success.'
+else
+    rclone config reconnect gdrive:
+    exit 1
+fi" > ~/bin/keepassxc-sync
+chmod +x ~/bin/keepassxc-sync
+
+echo "you can now sync the passwords keepass database with 'keepassxc-sync'"
 echo "you can now push or pull files inside ~/gdrive-shared with 'gdrive push' or 'gdrive pull'"
