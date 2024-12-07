31ddd8a1899a18ea7b4515a47b3353bd8c874502
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2024-12-07 13:31:34 -0800

    add sagemath

--- install-apps.sh
+++ install-apps.sh
@@ -44,3 +44,15 @@ curl -fsSL https://download.opensuse.org/repositories/home:justkidding/xUbuntu_2
 sudo apt update
 sudo apt install ueberzugpp
 
+# sagemath
+sudo apt install -y automake bc binutils bzip2 ca-certificates cliquer cmake curl ecl eclib-tools fflas-ffpack flintqs g++ gengetopt gfan gfortran git glpk-utils gmp-ecm lcalc libatomic-ops-dev libboost-dev libbraiding-dev libbz2-dev libcdd-dev libcdd-tools libcliquer-dev libcurl4-openssl-dev libec-dev libecm-dev libffi-dev libflint-dev libfreetype-dev libgc-dev libgd-dev libgf2x-dev libgiac-dev libgivaro-dev libglpk-dev libgmp-dev libgsl-dev libhomfly-dev libiml-dev liblfunction-dev liblrcalc-dev liblzma-dev libm4rie-dev libmpc-dev libmpfi-dev libmpfr-dev libncurses-dev libntl-dev libopenblas-dev libpari-dev libpcre3-dev libplanarity-dev libppl-dev libprimesieve-dev libpython3-dev libqhull-dev libreadline-dev librw-dev libsingular4-dev libsqlite3-dev libssl-dev libsuitesparse-dev libsymmetrica2-dev zlib1g-dev libzmq3-dev libzn-poly-dev m4 make nauty openssl palp pari-doc pari-elldata pari-galdata pari-galpol pari-gp2c pari-seadata patch perl pkg-config planarity ppl-dev python3-setuptools python3-venv r-base-dev r-cran-lattice singular sqlite3 sympow tachyon tar tox xcas xz-utils
+sudo apt install -y texlive-latex-extra texlive-xetex latexmk pandoc dvipng
+sudo apt install -y libec-dev eclib-tools fflas-ffpack libfplll-dev gap libgap-dev libgivaro-dev texinfo liblinbox-dev liblrcalc-dev libm4ri-dev libm4rie-dev maxima meson ninja-build patchelf libprimecount-dev qhull-bin libqhull-dev singular singular-doc libsingular4-dev
+
+git clone --branch master https://github.com/sagemath/sage.git --depth 1 ~/sage
+cd ~/sage
+make configure
+./configure
+MAKE="make -j8" make
+sudo ln -sf ~/sage /usr/local/bin
+
